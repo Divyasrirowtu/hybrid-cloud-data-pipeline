aed1d61f3db8ac3c57733dfd56672aeb73d0b7ba
Divya sri rowtu <divyasrirowtu@gmail.com>
2026-02-18 18:34:23 +0530

    Configure Terraform providers for LocalStack and GCP

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "gcp_project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "gcp_region" {
+  description = "GCP Region"
+  type        = string
+}
+
+variable "gcp_keyfile_path" {
+  description = "Path to GCP service account JSON key"
+  type        = string
+}
